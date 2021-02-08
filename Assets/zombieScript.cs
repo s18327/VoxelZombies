31ebdad534ff85e4568a049f7d402690aff3fc18
Jakub Góralczyk <s18327@users.noreply.github.com>
2021-02-08 13:09:26 +0100

    second commit added zombie with animations

---
+++ Assets/zombieScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class zombieScript : MonoBehaviour
+{
+    public Transform Player;
+    int MoveSpeed = 2;
+    int MaxDist =15;
+    int MinDist = 4;
+    int FastMoveSpeed = 3;
+    Animator anim;
+    Rigidbody rb;
+
+
+
+    void Start()
+    {
+         rb = GetComponent<Rigidbody>();
+         anim = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        Vector3 direction = Player.position - transform.position;
+        direction.Normalize();
+        direction.y = 0;
+        if(Vector3.Distance(transform.position, Player.position) >= MaxDist) { 
+            direction.Equals(0);
+            
+            anim.SetFloat("Blend", 0f); 
+
+        }
+
+        else if (Vector3.Distance(transform.position, Player.position) <= MaxDist&& Vector3.Distance(transform.position, Player.position) > MinDist)
+        {
+            transform.LookAt(Player);
+
+            anim.SetFloat("Blend",0.5f);
+            // transform.position += direction * MoveSpeed * Time.smoothDeltaTime;
+            rb.MovePosition((Vector3)transform.position + (direction * MoveSpeed * Time.smoothDeltaTime));
+        }
+        else if (Vector3.Distance(transform.position, Player.position) <= MinDist) {
+            transform.LookAt(Player);
+            //transform.position += direction * FastMoveSpeed * Time.smoothDeltaTime;
+            anim.SetFloat("Blend",0.98f);
+            rb.MovePosition((Vector3)transform.position + (direction * FastMoveSpeed * Time.smoothDeltaTime));
+            
+        }
+    }
+
+
+
+}
